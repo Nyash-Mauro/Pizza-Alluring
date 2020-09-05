7d696889da088b47528082d6e58b3204cb605b89
Nyash-Mauro <mauricee423@gmail.com>
2020-09-05 23:40:14 +0300

    added case to the to the size

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ function GetPizza(name,size,crust,topping, total){
     this.topping =topping;
     this.total =total;
 }
+
+// submit button
+$(document).ready(function(){})
+$("button.submit").click(function(event){
+    let pname = $(".name option:selected").val();
+    let psize = $("#size option:selected").val();
+    let pcrust = $("#crust option:selected").val();
+    let ptopping = [];
+    ptopping.push($(this).val());
+});
+console.log(ptopping.join(","));
+
+switch (psize) {
+    case "0":
+        price =0
+        break;
+    case "large":
+        price=1500;
+        console.log(price);
+        break;
+    case "medium":
+        price=800;
+        console.log("Price is"
+        +price)
+        break;
+    case "small":
+        price=600;
+        console.log(price)
+    default:
+        break;
+}
